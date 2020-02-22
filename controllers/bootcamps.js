aaa367d2392437d461864d0df2c7e6412837873f
recepdoganp <rec.do.pezukoglu@hotmail.com>
2020-02-22 23:25:14 +0100

    seeder.js created for bootcamps

---
+++ controllers/bootcamps.js
@@ -0,0 +1,80 @@
+const ErrorResponse = require("../utils/errorResponse");
+const asyncHandler = require("..//middleware/async");
+const Bootcamp = require("../modals/Bootcamp");
+
+// @desc    Get all bootcamps
+// @route   GET /api/v1/bootcamps
+// @access  Public
+exports.getBootcamps = asyncHandler(async (req, res, next) => {
+  const bootcamps = await Bootcamp.find();
+  res.status(200).json({
+    success: true,
+    count: bootcamps.length,
+    data: bootcamps
+  });
+});
+
+// @desc    Get a single bootcamp
+// @route   GET /api/v1/bootcamps/:id
+// @access  Public
+exports.getBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findById(req.params.id);
+  // handle correctly formatted nonexisting ids, because it will yield a result but with null
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: bootcamp
+  });
+});
+
+// @desc    Create a single bootcamp
+// @route   POST /api/v1/bootcamps
+// @access  Private
+exports.createBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.create(req.body);
+  res.status(201).json({
+    success: true,
+    data: bootcamp
+  });
+});
+
+// @desc    Update a single bootcamp
+// @route   PUT /api/v1/bootcamps/:id
+// @access  Private
+exports.updateBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
+
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: bootcamp
+  });
+});
+
+// @desc    Delete a single bootcamp
+// @route   DELETE /api/v1/bootcamps/:id
+// @access  Private
+exports.deleteBootcamp = asyncHandler(async (req, res, next) => {
+  const bootcamp = await Bootcamp.findByIdAndRemove(req.params.id);
+  if (!bootcamp) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404)
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: {}
+  });
+});
---
+++ seeder.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const mongoose = require("mongoose");
+const colors = require("colors");
+const dotenv = require("dotenv");
+
+// Load env vars
+
+dotenv.config({ path: "./config/config.env" });
+
+// load modals
+const Bootcamp = require("./modals/Bootcamp");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, "utf-8")
+);
+
+// import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log(`Data imported...`.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+    console.log(`Data destroyed...`.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// assign node arguments for which action to perform
+if (process.argv[2] === "-i") {
+  // if argument -i is passed then import the data
+  importData();
+} else if (process.argv[2] === "-d") {
+  deleteData();
+}
